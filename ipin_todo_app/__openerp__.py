95f46bebb089faefb5018ed8800e4e5ec34c51a7
EACADEMY <EACADEMY>
2016-02-04 11:53:36 +0700

    commit pertama odoo

---
+++ ipin_todo_app/__openerp__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Arifin To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Arifin',
+    'depends': ['mail'],
+    'application': True,
+    'data': ['todo_view.xml'],
+}
